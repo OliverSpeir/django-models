44997cd71b075d08fab370794080fe20768349e7
Oliver Speir <oliverspeir9@gmail.com>
2023-01-24 18:42:57 -0700

    lab27

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Snack
+from django.contrib.auth import get_user_model
 
 
 class SnacksTest(TestCase):
@@ -14,3 +16,37 @@ class SnacksTest(TestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'snack_list.html')
         self.assertTemplateUsed(response, 'base.html')
+
+    def setUp(self):
+        reviewer = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="rake", purchaser=reviewer)
+
+    def test_list_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+
+    def test_detail_page_status_code(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page_template(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_detail.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_detail_page_context(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        snack = response.context['snack']
+        self.assertEqual(snack.name, "rake")
+        self.assertEqual(snack.purchaser.username, "tester")
